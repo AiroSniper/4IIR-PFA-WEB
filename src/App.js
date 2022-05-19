8f6da7708601b3d51137116ce537e461d2569468
youbchaf <youbchaf@gmail.com>
2022-05-19 23:11:25 +0100

    fragment commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import React from "react";
 function App() {
   return (
     <React.Fragment>
-        
+        <div class="_front">
+        <div class="notice">
+            <div class="instr">
+                <h6 class="inst_title">Try to select all edges</h6>
+                <h6 class="inst">Good luck</h6>
+            </div>
+
+            <div class="img">
+                <img src="./robot.png" alt=""/>
+            </div>
+        </div>
+        <div class="_container">
+          <Canvas/>
+           
+        </div>
+    </div>
+    <div class="_background">
+
+        <div class="_top">
+
+        </div>
+
+        <div class="_bottom">
+            
+        </div>
+    </div>
+
   
      </React.Fragment>
   );
